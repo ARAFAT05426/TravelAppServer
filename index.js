cdb1a36b1a1b1a91376c668854d6fe0949ac1b9d
ARAFAT05426 <arafat05426@gmail.com>
2024-04-27 08:49:43 +0600

    added post and find functionality

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cors = require('cors');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5426;
 
 // Middleware to parse JSON bodies
-app.use(cors)
+app.use(cors())
 app.use(express.json());
 
-// Example route
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b5qg8rw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const spotsDB = client.db("spotsDB");
+    const spotsCollection = spotsDB.collection("spotsCollection");
+    // CREATE OF CRUD
+    app.post('/spots', async(req, res) =>{
+      const data = req.body;
+      const dbResponse = await spotsCollection.insertOne(data)
+      res.send(dbResponse)
+    })
+    // READ OF CRUD
+    app.get('/spots', async(req, res) =>{
+      const dbResponse = await spotsCollection.find().toArray();
+      res.send(dbResponse)
+    })
+    // READ-ONE OF CRUD 
+    app.get('/spots/:id', async(req,res) =>{
+      const id = new ObjectId(req.params.id);
+      const dbResponse = await spotsCollection.findOne(id);
+      res.send(dbResponse)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Route
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 // Start server
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
